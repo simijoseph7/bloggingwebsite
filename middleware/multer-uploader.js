b53d3b087b228db3ccb9ce6bfd1581a03e820397
SimiJoseph <87762212+simijoseph7@users.noreply.github.com>
2022-10-08 12:52:12 +1300

    Add files via upload

---
+++ middleware/multer-uploader.js
@@ -0,0 +1,10 @@
+// Setup multer (files will temporarily be saved in the "temp" folder).
+
+const path = require("path");
+const multer = require("multer");
+const upload = multer({
+    dest: path.join(__dirname, "../public/images/temp")
+});
+
+// Export the "upload" object, which we can use to actually accept file uploads.
+module.exports = upload;
