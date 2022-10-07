191e256c0b626e266ce2b40338e7c724e1c1a354
SimiJoseph <87762212+simijoseph7@users.noreply.github.com>
2022-10-08 12:51:39 +1300

    Update auth-middleware.js

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -1,1 +1,56 @@
+const {retrieveUserWithAuthToken} = require("../modules/user-dao.js");
+const {
+    fetchAllCommentByArticleId,
+    fetchAllArticle,
+    displayTime,
+    fetchArticlesByCategory,
+    fetchRecentArticle
+} = require("../modules/articles-dao.js.js");
 
+
+async function addUserToLocals(req, res, next) {
+const user = await retrieveUserWithAuthToken(req.cookies.authToken);
+  res.locals.user = user;
+  next();
+}
+
+async function verifyAuthenticated(req, res, next) {
+    if (res.locals.user) {
+        next();
+    }
+    else {
+          const recentArticle = await fetchRecentArticle()
+         const articles = await fetchAllArticle();
+         if (articles) {
+             articles.forEach(async article => {
+                 article.creationDate = displayTime(new Date(article.creationDate))
+                 const allComments = await fetchAllCommentByArticleId(article.articleId)
+                 if (allComments != []) {
+                     allComments.forEach(comment => {
+                         comment.creationDate = displayTime(new Date(comment.creationDate))
+                     })
+                     article.comments = allComments;
+                     article.commentsCount = allComments.length;
+                     
+                 }
+             })
+
+             
+             res.locals.allArticles = articles;
+
+         }
+         if (recentArticle) {
+             recentArticle.content = (recentArticle.content+"").substring(0,200)
+             
+             recentArticle.creationDate = displayTime(new Date(recentArticle.creationDate))
+             res.locals.recentPost = recentArticle;
+
+         }
+         res.render("home")
+    }
+}
+
+module.exports = {
+    addUserToLocals,
+    verifyAuthenticated
+}
